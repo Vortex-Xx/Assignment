bd5ff50699d22b0b72a55c7d605e03a636105b19
Shahriar Mahir <shahriarmahir114@gmail.com>
2025-07-29 21:29:09 +0600

    Reaction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function AuthorProfile() {
   return (
     <div className="profile-card">
-      <div className="circle profile-avatar"></div>
+      <div className="circle"></div>
       <h2>Author Name</h2>
       <p>Lorem Ipsum Dolor</p>
+    </div>
+  );
+}
+
+function ReactionsBar() {
+  const reactions = [
+    { label: 'Like', percent: '20%' },
+    { label: 'Love', percent: '60%' },
+    { label: 'Angry', percent: '5%' },
+    { label: 'Sad', percent: '5%' },
+  ];
+  const [active, setActive] = useState(null);
+
+  return (
+    <div className="reactions-bar">
+      {reactions.map((r, idx) => (
+        <div
+          key={r.label}
+          className={`reaction${active === idx ? ' active' : ''}`}
+          onClick={() => setActive(idx)}
+        >
+          <div className="reaction-label">{r.label}</div>
+          <div className="reaction-percent">{r.percent}</div>
+        </div>
+      ))}
     </div>
   );
 }
@@ -27,6 +53,10 @@ function Home() {
           <div className="author-date">7 January 2025</div>
         </div>
        </div>
+       <hr className="divider" />
+       <p className='para3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet animi facere inventore officia perspiciatis molestias esse ducimus. Illum eveniet provident odit, iusto, quidem harum placeat veniam voluptate, pariatur ad inventore? Recusandae ipsum reprehenderit cupiditate. At, ratione aspernatur eum repellendus totam eligendi quos dolor vel sint. Exercitationem quidem vel iure eius Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur corrupti corporis ipsam ea ratione sequi officiis, libero voluptatibus magni error inventore maxime in. Numquam illo reprehenderit repellendus repellat rerum, excepturi iure consectetur minima cumque eaque odit fuga magni ullam, explicabo tempore? Alias nulla cumque ab, repudiandae quis exercitationem facilis, sint consequuntur nesciunt in iure maxime dignissimos vero unde! Sint adipisci dignissimos ullam culpa, illo dolores rem mollitia quidem inventore iure accusantium quisquam unde consequuntur fugit, rerum earum quo ea necessitatibus tenetur error sunt nisi! Repudiandae distinctio omnis praesentium laborum, corporis cupiditate nihil odit vel, debitis corrupti, facere consectetur! Dolore, magni.</p>
+       <hr className="divider" />
+        <ReactionsBar />
     </div>
   );
 }
